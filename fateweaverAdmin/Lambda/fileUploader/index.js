74f6b3cc7944c213a9f4ce56d942aaa78dd64715
John <john@blooware.co.uk>
2019-03-13 11:15:24 +0000

    Working Csv File Uploader

--- fateweaverAdmin/Lambda/fileUploader/index.js
+++ fateweaverAdmin/Lambda/fileUploader/index.js
@@ -42,13 +42,6 @@ var connection = mysql.createConnection({
     password: 'blooware18',
     port: 3306
 });
-/*
-async function insertData(NewData, file) {
-    var x = await insertTutorGroup(NewData, file);
-    //x = results.insertId
-    console.log("Completed creation of students and Tutor Groups (If needed)");
-}
-*/
 
 exports.handler = (event, context, callback) => {
     //callback(null,event);
@@ -90,70 +83,28 @@ exports.handler = (event, context, callback) => {
                             errMsg: "Couldn't find any data in the CSV file"
                         });
                     } else {
-
-                        // if the data fields are not in the list then return the name of the feild that is incorrect
-                        /*
-                        for (var i = 0; i < fields.length; i++) {
-                            if (JsonData[0][fields[i]] != null) {
-                                console.log(fields[i]);
-                            } else {
-                                callback(null, {
-                                    statusCode: 200,
-                                    status: false,
-                                    errMsg: "couldn't find the field named " + fields[i],
-                                });
-                            }
-                        }
-                        */
-                        //processArray(JsonData);
                         processFields(fields, JsonData);
-                        
-                        // For Each Row
-                        //console.log(JsonData);
                     }
                 }
             }
         });
     });
-    
-    var Added = {};
-    var notAdded ={} ;
+
+    var Added = [];
+    var notAdded = [];
+    var TutorGroupsAdded = [];
     //callback(null, "List");
     function delay() {
         return new Promise(resolve => setTimeout(resolve, 300));
     }
 
     async function delayedJsonData(item) {
-        // notice that we can await a function
-        // that returns a promise
-        /*
-        if(item === 2){
-            notDone += item;
-            var dset = {
-                name : item,
-                info : "Not Done"
-            }
-        } else {
-            Done += item;
-            var dset = {
-                name : item,
-                info : "Done"
-            }
-            console.log("added Done Item");
-            connection.query("insert into fateweaver.students set ?", [dset], async function (error, results, fields) {
-                console.log("added Not done item");
-                console.log(results);
-            });
-            
-        }
-        */
         // This is where i would add them to mysql
-        
+
         connection.query("select * from fateweaver.tutor_groups where name = ?", [item.TutorGroup], function (err, results, fields) {
             if (err) {
                 console.log("Error getting tutor groups:", err);
             }
-
 
             if (results.length == 0) {
                 var dataGroup = {
@@ -165,84 +116,47 @@ exports.handler = (event, context, callback) => {
                 }
                 connection.query("insert into fateweaver.tutor_groups set ? ", dataGroup, function (error, results, fields) {
 
-                    //CreateStudent(results.insertId, NewData, file.fileFullName, function () {
-
-                        console.log("Student added - There Wasn't a tutor Group");
-                        console.log(results.insertId);
-                        //now create a student using this group id
-                        createStudent(results.insertId, item, file);
-                    //});
+                    console.log("Student added - There Wasn't a tutor Group");
+                    console.log(results.insertId);
+                    //now create a student using this group id
+                    TutorGroupsAdded.push(item.TutorGroup);
+                    createStudent(results.insertId, item, file);
 
                 });
             } else {
-
-                //CreateStudent(results[0].id, NewData, file.fileFullName, function () {
-                    console.log("Student added - There Was a tutor Group");
-                    console.log(results[0].id);
-                    createStudent(results[0].id, item, file);
-                    //now create a student using this group id
-
-                   //resolve(null);
-               // });
+                console.log("Student added - There Was a tutor Group");
+                console.log(results[0].id);
+                createStudent(results[0].id, item, file);
+                //now create a student using this group id
 
             }
         });
-        
-        
-        
-        
-        
-        //console.log("The items are :");
-        //console.log(item);
+
         await delay();
     }
     async function processJsonData(array) {
         for (const item of array) {
-          await delayedJsonData(item);
+            await delayedJsonData(item);
         }
-        
-        
+
+
         console.log("Finished Itterating");
-        context.succeed({Done : Added,
-            NotDone : notAdded});
-        callback(null, {
-            Done : Added,
-            NotDone : notAdded,
-             
+        context.succeed({
+            Done: Added,
+            NotDone: notAdded,
+            TutorGroupsAdded: TutorGroupsAdded
+
         });
     }
-    
-    
-    
+
+
+
     function delay() {
         return new Promise(resolve => setTimeout(resolve, 300));
     }
 
     async function delayedFields(field, JsonData) {
-        // notice that we can await a function
-        // that returns a promise
-        /*
-        if(item === 2){
-            notDone += item;
-            var dset = {
-                name : item,
-                info : "Not Done"
-            }
-        } else {
-            Done += item;
-            var dset = {
-                name : item,
-                info : "Done"
-            }
-            console.log("added Done Item");
-            connection.query("insert into fateweaver.students set ?", [dset], async function (error, results, fields) {
-                console.log("added Not done item");
-                console.log(results);
-            });
-            
-        }
-        */
-        
+
         if (JsonData[0][field] != null) {
             console.log(field);
         } else {
@@ -252,26 +166,30 @@ exports.handler = (event, context, callback) => {
                 errMsg: "couldn't find the field named " + field,
             });
         }
-        
         console.log(JsonData)
-        
-        
         await delay();
         console.log(fields);
     }
     async function processFields(array, JsonData) {
         for (const item of array) {
-          await delayedFields(item, JsonData);
+            await delayedFields(item, JsonData);
         }
         console.log("Finished Itterating through the fields");
         console.log("Now i would do this json data :")
         console.log(JsonData);
         processJsonData(JsonData);
         //Now process the students
-        
-      }
-    
-    async function createStudent(Group_id, StudentInfo, file){
+
+    }
+
+    async function createStudent(Group_id, StudentInfo, file) {
+        var jsonStudent = {
+            Name: StudentInfo.Name,
+            Info: StudentInfo.Info,
+            Age: StudentInfo.Age,
+            TutorGroup: StudentInfo.TutorGroup,
+        }
+
         var dset = {
             name: StudentInfo.Name,
             info: StudentInfo.Info,
@@ -281,31 +199,36 @@ exports.handler = (event, context, callback) => {
             added_id: "3001",
             csv: file.fileFullName
         }
-        
+
         connection.query("select * from fateweaver.students where name = ? and info = ? and age = ?  ", [StudentInfo.Name, StudentInfo.Info, StudentInfo.Age], function (error, results, fields) {
-            if (results.length > 0){
+            if (results.length > 0) {
                 //append to Not added
-                notAdded += dset
+                var jsonStudent = {
+                    Name: StudentInfo.Name,
+                    Info: StudentInfo.Info,
+                    Age: StudentInfo.Age,
+                    TutorGroup: StudentInfo.TutorGroup,
+                    Duplicate: results[0].id,
+                }
+                notAdded.push({ jsonStudent });
             } else {
                 connection.query("insert into fateweaver.students set ?", [dset], function (error, results, fields) {
-                    Added += dset
+                    var jsonStudent = {
+                        Name: StudentInfo.Name,
+                        Info: StudentInfo.Info,
+                        Age: StudentInfo.Age,
+                        TutorGroup: StudentInfo.TutorGroup,
+                    }
+                    Added.push({ jsonStudent });
                     console.log(results);
                 });
-                
+
             }
         });
-        
-        
+
         console.log(Group_id);
         console.log(StudentInfo);
-        
     }
-    
-    
-    
-    
-    
-    
 
 }
 
@@ -355,71 +278,3 @@ function csvTojs(csv) {
     }
     return result;
 }
-/*
-function insertTutorGroup(NewData, file) {
-    return new Promise(resolve => {
-        var x = NewData.TutorGroup;
-        //resolve(null);
-
-
-        connection.query("select * from fateweaver.tutor_groups where name = ?", [x], function (err, results, fields) {
-            if (err) {
-                console.log("Error getting tutor groups:", err);
-            }
-            connection.end(function (err) {
-                if (err) {
-                    console.log("Error ending the connection:", err);
-                }
-
-                if (results.length == 0) {
-                    var dataGroup = {
-                        name: x,
-                        description: "Auto Generated By Upload",
-                        added: new Date(Date.now()),
-                        added_id: "3001",
-                        csv: file.fileFullName
-                    }
-                    connection.query("insert into fateweaver.tutor_groups set ? ", dataGroup, function (error, results, fields) {
-                        connection.end(function (err) {
-                            if (err) {
-                                console.log("Error ending the connection:", err);
-                            }
-                            console.log("There Wasn't a tutor Group");
-                            CreateStudent(results.insertId, NewData, file.fileFullName, function () {
-
-                            });
-                        });
-                        resolve(results.insertId);
-
-                    });
-                } else {
-                    console.log("There Was a tutor Group");
-                    CreateStudent(results[0].id, NewData, file.fileFullName, function () {
-
-                    });
-                    resolve(results[0].id);
-
-                }
-            });
-        });
-
-    });
-
-}
-function CreateStudent(Group_id, Data, fileFullName, callback2) {
-    var set = {
-        name: Data.Name,
-        info: Data.Info,
-        age: Data.Age,
-        tutor_group_id: Group_id,
-        added: new Date(Date.now()),
-        added_id: "3001",
-        csv: fileFullName
-    }
-    console.log(set);
-    connection.query("insert into fateweaver.students set ?", [set], function (error, results, fields) {
-
-    });
-    callback2();
-}
-*/
