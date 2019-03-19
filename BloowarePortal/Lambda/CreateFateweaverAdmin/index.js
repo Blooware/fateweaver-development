bfa03aeb3c5cda717ef429a59f617ffab4bbe625
John <john@blooware.co.uk>
2019-03-19 11:47:12 +0000

    working add sub

--- BloowarePortal/Lambda/CreateFateweaverAdmin/index.js
+++ BloowarePortal/Lambda/CreateFateweaverAdmin/index.js
@@ -15,8 +15,8 @@ var connection = mysql.createConnection({
 exports.handler = (event, context, callback) => {
     console.log(event);
     //callback(null, event);
-    
-    
+
+
     /*
     connection.query("select * from fateweaver.schools", [data], function (err, results, fields) {
         if (err) {
@@ -38,8 +38,8 @@ exports.handler = (event, context, callback) => {
         }
     });
     */
-    
-    
+
+
     var poolData = {
         UserPoolId: 'eu-west-2_9yPc9js2X',
         ClientId: '4atq7kp8m6ibv56d3cgjbudhim'
@@ -66,79 +66,44 @@ exports.handler = (event, context, callback) => {
     attributeList.push(attributePhoneNumber);
     //TODO this should be a temport password
 
-    userPool.signUp(event.form.email, "Event.form.password2!", attributeList, null, function (err, result) {
+    userPool.signUp(event.form.email, event.form.password, attributeList, null, function (err, result) {
+
         if (err) {
-            context.succeed({
-                statusCode: 200,
-                status: false,
-                err : err,
-                erroris : "The error is here1"
-            });
+            console.log(err);
             return;
         }
-        cognitoUser = result.user;
-        console.log("Stuff" + cognitoUser);
-        var CognitoDataStuff;
-        callback(null, {
-            cognitoUser: cognitoUser,
-            results : results,
-            resultssub : results.sub,
-        })
-        //console.log('user name is ' + cognitoUser.getUsername());
-        /*
-        cognitoUser.getUserAttributes(function(err, result) {
 
+        //cognitoUser = result.user;
+        console.log(JSON.stringify(result.userSub));
+
+        var data = {
+            email: event.form.email,
+            cognito_id: result.userSub,
+            added: new Date(Date.now()),
+            added_id: event.account.sub,
+            school_id: event.form.school_id
+        }
+        connection.query("insert into fateweaver.admins set ?", [data], function (err, results, fields) {
             if (err) {
+                console.log("Error getting tutor groups:", err);
                 context.succeed({
                     statusCode: 200,
                     status: false,
-                    err : err,
-                    erroris : "The error is here2"
+                    errMsg: "error adding that mentor err :" + err
                 });
             }
-            CognitoDataStuff = result;
 
-            var data = {
-                email: event.form.email,
-                cognito_id: "result.user.sub",
-                added: new Date(Date.now()),
-                added_id : event.account.sub,
-                school_id: event.form.school_id
-            }
-            connection.query("insert into fateweaver.admins set ?", [data], function (err, results, fields) {
-                if (err) {
-                    console.log("Error getting tutor groups:", err);
-                    context.succeed({
-                        statusCode: 200,
-                        status: false,
-                        errMsg: "error adding that mentor err :" + err
-                    });
-                }
-                
-                context.succeed({
-                    statusCode: 200,
-                    status: true,
-                    body : results,
-                    User : cognitoUser,
-                    CognitoDataStuff : CognitoDataStuff
-    
-                });
+            context.succeed({
+                statusCode: 200,
+                status: true,
+                body: results,
+
             });
-            
         });
-        */
-        
-
-        
-
-
-        
-        
-        
 
     });
-    
-    
+
+
 
 
 }
