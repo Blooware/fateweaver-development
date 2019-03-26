7acbcfdff56c57e0a32c85ac658475b6cac17c61
John <john@blooware.co.uk>
2019-03-26 15:54:12 +0000

    loading in tutors and searching them

--- fateweaverAdmin/Lambda/getStudents/index.js
+++ fateweaverAdmin/Lambda/getStudents/index.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
 
 exports.handler = (event, context, callback) => {
 
-    connection.query("select *, fateweaver.students.id as student_id, fateweaver.tutor_groups.name as tutorGroup, (select aspiration from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1) as aspiration, (select if(length((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)) > 1,((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)),((select concat('(Unconfirmed) ', aspiration) from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)))) as destination from fateweaver.students left join fateweaver.tutor_groups on fateweaver.tutor_groups.id=fateweaver.students.tutor_group_id where fateweaver.students.school_id = (select school_id from fateweaver.admins where cognito_id = ? limit 1);", [event.account.sub], function (err, results, fields) {
+    connection.query("select *, if (fateweaver.students.year is not null , if((DATE_FORMAT(now(), '%m')) < 9, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year + 1),    (select if (DATE_FORMAT(dob, '%m') < 9, (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 4 ) , (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 5 ) ))) as yearGroup, fateweaver.students.id as student_id, fateweaver.tutor_groups.name as tutorGroup, (select aspiration from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1) as aspiration, (select if(length((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)) > 1,((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)),((select concat('(Unconfirmed) ', aspiration) from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)))) as destination from fateweaver.students left join fateweaver.tutor_groups on fateweaver.tutor_groups.id=fateweaver.students.tutor_group_id where fateweaver.students.school_id = (select school_id from fateweaver.admins where cognito_id = ? limit 1);", [event.account.sub], function (err, results, fields) {
         if (err) {
             console.log("Error getting tutor groups:", err);
             context.succeed({
@@ -22,17 +22,38 @@ exports.handler = (event, context, callback) => {
                 errMsg: "error getting students :" + err
             });
         }
-        context.succeed({
-            statusCode: 200,
-            status: true,
-            body: results
+        var studentInfo = results;
+        connection.query("select * from fateweaver.tutor_groups where school_id = (select school_id from fateweaver.admins where cognito_id = ? limit 1); ", [event.account.sub], function (err, results, fields) {
+            if (err) {
+                console.log("Error getting tutor groups:", err);
+                context.succeed({
+                    statusCode: 200,
+                    status: false,
+                    errMsg: "error getting students :" + err
+                });
+            }
+            context.succeed({
+                statusCode: 200,
+                status: true,
+                body: studentInfo,
+                tutor_groups: results
+            });
+
         });
+
+
     });
 }
 
 
 
-
+/*
+context.succeed({
+            statusCode: 200,
+            status: true,
+            body: results
+        });
+        */
 
 
 
