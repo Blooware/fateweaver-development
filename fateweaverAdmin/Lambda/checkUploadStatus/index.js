79afdff10d5b6054b2a604d3b97773d57d3da6ce
unknown <vfxgames2014@gmail.com>
2019-04-04 12:17:04 +0100

    uploadprogress bar working

---
+++ fateweaverAdmin/Lambda/checkUploadStatus/index.js
@@ -0,0 +1,39 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    "host": process.env.host,
+    "user": process.env.user,
+    "password": process.env.password,
+    "port": process.env.port
+});
+
+exports.handler = (event, context, callback) => {
+
+    connection.query("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));", [event.account.sub], function (err, results, fields) {
+        connection.query("select * from fateweaver.upload_progress where sub = ? order by id desc limit 1 ", [event.account.sub], function (err, results, fields) {
+            if (err) {
+                console.log("Error getting tutor groups:", err);
+                context.succeed({
+                    statusCode: 200,
+                    status: false,
+                    errMsg: "error getting students"
+                });
+            }
+
+            context.succeed({
+                statusCode: 200,
+                status: true,
+                body: results
+            });
+        });
+    });
+
+}
+
+
+
+
+
+
+
+
+//john was here
