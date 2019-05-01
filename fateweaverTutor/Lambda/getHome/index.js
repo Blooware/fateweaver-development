9fd59c7f69b5fd73a7a46a2f9c70346e88f02457
John <john@blooware.co.uk>
2019-05-01 16:36:02 +0100

    loads tutors groups

---
+++ fateweaverTutor/Lambda/getHome/index.js
@@ -0,0 +1,32 @@
+//fateweaverTutor-getHome
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    "host": process.env.host,
+    "user": process.env.user,
+    "password": process.env.password,
+    "port": process.env.port
+});
+
+exports.handler = (event, context, callback) => {
+    //callback(null, event);
+    
+    connection.query("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));", [event.account.sub], function (err, results, fields) {
+        connection.query("select id, name, description, added  from fateweaver.tutor_groups where id in (select group_id from tutor_assigned_groups where tutor_id = (select id from fateweaver.tutors where cognito_id = ?) and school_id = (select school_id from fateweaver.tutors where cognito_id = ?))", [event.account.sub, event.account.sub], function (err, results, fields) {
+            if (err) {
+                console.log("Error getting tutor groups:", err);
+                context.succeed({
+                    statusCode: 200,
+                    status: false,
+                    errMsg: "error getting Tutor Groups"
+                });
+            }
+            context.succeed({
+                statusCode: 200,
+                status: true,
+                body: results
+            });
+        });
+    });
+    
+}
