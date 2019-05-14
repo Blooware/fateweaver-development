f914abc42da685b54843c1c7abff085b31511b99
John <john@blooware.co.uk>
2019-05-14 16:40:16 +0100

    start to assign student to mentor

--- fateweaverAdmin/Lambda/GrabStudent/index.js
+++ fateweaverAdmin/Lambda/GrabStudent/index.js
@@ -1,3 +1,4 @@
+//fateweaverAdmin-postGrabStudent
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     "host": process.env.host,
@@ -8,9 +9,8 @@ var connection = mysql.createConnection({
 
 exports.handler = (event, context, callback) => {
     //callback(null,event);
-
     connection.query("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));", [event.account.sub], function (err, results, fields) {
-        connection.query("select *, DATE_FORMAT(added, '%d/%m/%Y') as added, if (fateweaver.students.year is not null , if((DATE_FORMAT(now(), '%m')) < 9, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year + 1),    (select if (DATE_FORMAT(dob, '%m') < 9, (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 4 ) , (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 5 ) ))) as yearGroup, DATE_FORMAT(fateweaver.students.dob, '%Y-%m-%d') as dateOfBirth, fateweaver.tutor_groups.name as tutorGroup from fateweaver.students left join fateweaver.tutor_groups on fateweaver.tutor_groups.id=fateweaver.students.tutor_group_id where fateweaver.students.id = ? and fateweaver.students.school_id = (select school_id from fateweaver.admins where cognito_id = ? limit 1)", [event.form.student_id, event.account.sub], function (err, results, fields) {
+        connection.query("select *, DATE_FORMAT(fateweaver.students.added, '%d/%m/%Y') as added, if (fateweaver.students.year is not null , if((DATE_FORMAT(now(), '%m')) < 9, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year + 1),    (select if (DATE_FORMAT(dob, '%m') < 9, (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 4 ) , (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 5 ) ))) as yearGroup, DATE_FORMAT(fateweaver.students.dob, '%Y-%m-%d') as dateOfBirth, fateweaver.tutor_groups.name as tutorGroup from fateweaver.students left join fateweaver.tutor_groups on fateweaver.tutor_groups.id=fateweaver.students.tutor_group_id where fateweaver.students.id = ? and fateweaver.students.school_id = (select school_id from fateweaver.admins where cognito_id = ? limit 1)", [event.form.student_id, event.account.sub], function (err, results, fields) {
             if (err) {
                 console.log("Error getting tutor groups:", err);
                 context.succeed({
@@ -51,16 +51,27 @@ exports.handler = (event, context, callback) => {
                         }
                         var studentInterests = results;
 
+                        connection.query("select id, first_name, last_name, role, linkedin, fateweaver.mentors.added  from fateweaver.mentors where school_id = (select school_id from fateweaver.admins where cognito_id = ? limit 1)", [event.account.sub], function (err, results, fields) {
+                            if (err) {
+                                console.log("Error getting tutor groups:", err);
+                                context.succeed({
+                                    statusCode: 200,
+                                    status: false,
+                                    errMsg: "error getting students :" + err
+                                });
+                            }
+                            var mentorList = results;
+                            //assigned mentors
 
-
-
-                        context.succeed({
-                            statusCode: 200,
-                            status: true,
-                            studentData: studentData,
-                            destinations: destinations,
-                            studentFiles: studentFiles,
-                            studentInterests: studentInterests,
+                            context.succeed({
+                                statusCode: 200,
+                                status: true,
+                                studentData: studentData,
+                                destinations: destinations,
+                                studentFiles: studentFiles,
+                                studentInterests: studentInterests,
+                                mentorList: mentorList,
+                            });
                         });
                     });
 
@@ -70,12 +81,3 @@ exports.handler = (event, context, callback) => {
     });
 
 }
-
-
-
-
-
-
-
-
-//john was here
---
+++ fateweaverAdmin/Lambda/postAssignStudentMentor/index.js
@@ -0,0 +1,20 @@
+//fateweaverAdmin-postAssignStudentMentor
+
+
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    "host": process.env.host,
+    "user": process.env.user,
+    "password": process.env.password,
+    "port": process.env.port
+});
+
+exports.handler = (event, context, callback) => {
+    
+    context.succeed({
+        event : event,
+    });
+    //assign them to a student / mentor new mysql table
+    
+}
