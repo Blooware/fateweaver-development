33361849d898070ea9372414919fb1e2bae9ae87
John <john@blooware.co.uk>
2019-05-15 16:45:53 +0100

    delete from s3

--- fateweaverAdmin/Lambda/DeleteStudent/index.js
+++ fateweaverAdmin/Lambda/DeleteStudent/index.js
@@ -1,5 +1,11 @@
 //fateweaverAdmin-postDeleteStudent
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const moment = require('moment');
+const fileType = require('file-type');
+const sha1 = require('sha1');
 var mysql = require('mysql');
+
 var connection = mysql.createConnection({
     "host": process.env.host,
     "user": process.env.user,
@@ -20,7 +26,33 @@ exports.handler = (event, context, callback) => {
                 });
             }
             if (results.length > 0) {
+
+                //Try to delete the files first
+                
+                var params = {
+                    Bucket: 'fateweaver-files',
+                    Key: '5151027b12542103e0e0999226c7d49cc9a82f78.png' // get path
+                  // where value for 'Key' equals 'pathName1/pathName2/.../pathNameN/fileName.ext' - full path name to your file without '/' at the beginning 
+                };
+                s3.deleteObject(params, function(err, data) {
+                    if (err) 
+                    context.succeed({
+                        error : err,
+                        stack : err.stack
+                    }); // an error occurred
+                    else     
+                    console.log(data);           // successful response
+                    context.succeed({ 
+                        deleted : "Deleted 5151027b12542103e0e0999226c7d49cc9a82f78.png",
+                        data : data,
+                
+                });
+                });
+                
+
                 //destination_sessions
+
+                /*
                 connection.query("delete from fateweaver.destination_sessions where student_id = ?", [event.form.student_id], function (err, results, fields) {
                     if (err) {
                         context.succeed({
@@ -40,11 +72,11 @@ exports.handler = (event, context, callback) => {
                         }
                         //student_files
                         //For each file delete it from s3
-                        
+
 
                     
                     });
-                });
+                });*/
 
                 //destination_sessions--
                 //mentor_assigned--
