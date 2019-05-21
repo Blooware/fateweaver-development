92ce52df20d4982aa4ea8365084db48376647704
John <john@blooware.co.uk>
2019-05-21 12:29:16 +0100

    working delete student
    deletes s3 files

--- fateweaverAdmin/Lambda/DeleteStudent/index.js
+++ fateweaverAdmin/Lambda/DeleteStudent/index.js
@@ -16,8 +16,82 @@ var connection = mysql.createConnection({
 exports.handler = (event, context, callback) => {
     var school_id;
 
-    connection.query("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));", [event.account.sub], function (err, results, fields) {
-        connection.query("select * from fateweaver.students where id = ? and school_id = (select school_id from fateweaver.admins where cognito_id = ?)", [event.form.student_id, event.account.sub], function (err, results, fields) {
+    connection.query("select * from fateweaver.students where id = ? and school_id = (select school_id from fateweaver.admins where cognito_id = ?)", [event.form.student_id, event.account.sub], function (err, results, fields) {
+        if (err) {
+            context.succeed({
+                statusCode: 200,
+                status: false,
+                errMsg: "error getting students :" + err
+            });
+        }
+        if (results.length > 0) {
+
+            //Try to delete the files first
+            //
+            connection.query("select * from fateweaver.student_files where student_id = ?", [event.form.student_id, event.account.sub], function (err, results, fields) {
+                if (err) {
+                    context.succeed({
+                        statusCode: 200,
+                        status: false,
+                        errMsg: "error getting students :" + err
+                    });
+                }
+
+                //delete files asyncronusly
+                deleteFiles(results)
+                async function deleteFiles(array) {
+                    for (const item of array) {
+                        await deleteFile(item);
+                    }
+                    console.log("Done");
+                    deleteTheRest();
+                    //Continue Deleting Stuff
+                }
+                async function deleteFile(item) {
+
+                    console.log("Deleteing" + item.name);
+                    var params = {
+                        Bucket: 'fateweaver-files',
+                        Key: item.path // get path
+                        // where value for 'Key' equals 'pathName1/pathName2/.../pathNameN/fileName.ext' - full path name to your file without '/' at the beginning 
+                    };
+                    s3.deleteObject(params, function (err, data) {
+                        if (err)
+                            context.succeed({
+                                error: err,
+                                stack: err.stack
+                            }); // an error occurred
+                        else
+                            console.log("deleting id :" + item.id)
+                        connection.query("delete from fateweaver.student_files where id = ? and student_id = ?", [item.id, event.form.student_id], function (err, results, fields) {
+                            if (err) {
+                                context.succeed({
+                                    statusCode: 200,
+                                    status: false,
+                                    errMsg: "error getting students :" + err
+                                });
+                            }
+                            console.log("delted id : " + item.id);
+                        });
+                    });
+                }
+
+            });
+
+        } else {
+            context.succeed({
+                statusCode: 200,
+                status: false,
+                errMsg: "You dont have access to Delete this student"
+            });
+        }
+
+    });
+    function deleteTheRest() {
+        console.log("Now im deleting the rest");
+        //destination_sessions
+        
+        connection.query("delete from fateweaver.destination_sessions where student_id = ?", [event.form.student_id], function (err, results, fields) {
             if (err) {
                 context.succeed({
                     statusCode: 200,
@@ -25,76 +99,17 @@ exports.handler = (event, context, callback) => {
                     errMsg: "error getting students :" + err
                 });
             }
-            if (results.length > 0) {
-
-                //Try to delete the files first
-                //
-                connection.query("select * from fateweaver.student_files where student_id = 13", [event.form.student_id, event.account.sub], function (err, results, fields) {
-                    if (err) {
-                        context.succeed({
-                            statusCode: 200,
-                            status: false,
-                            errMsg: "error getting students :" + err
-                        });
-                    }
-                    for (var i = 0; i < results.length; i++) {
-                        console.log("Deleting : " + results[i].path);
-
-                        var params = {
-                            Bucket: 'fateweaver-files',
-                            Key: results[i].path // get path
-                          // where value for 'Key' equals 'pathName1/pathName2/.../pathNameN/fileName.ext' - full path name to your file without '/' at the beginning 
-                        };
-                        s3.deleteObject(params, function(err, data) {
-                            if (err) 
-                            context.succeed({
-                                error : err,
-                                stack : err.stack
-                            }); // an error occurred
-                            else     
-                            console.log();           // successful response
-                            console.log("deleted : " + results[i].path)
-                        });
-                    }
-
-
-                });
-
-                
-
-                /*
-                var params = {
-                    Bucket: 'fateweaver-files',
-                    Key: '5151027b12542103e0e0999226c7d49cc9a82f78.png' // get path
-                  // where value for 'Key' equals 'pathName1/pathName2/.../pathNameN/fileName.ext' - full path name to your file without '/' at the beginning 
-                };
-                s3.deleteObject(params, function(err, data) {
-                    if (err) 
+            //mentor_assigned
+            connection.query("delete from fateweaver.mentor_assigned where student_id = ?", [event.form.student_id], function (err, results, fields) {
+                if (err) {
                     context.succeed({
-                        error : err,
-                        stack : err.stack
-                    }); // an error occurred
-                    else     
-                    console.log(data);           // successful response
-                    context.succeed({ 
-                        deleted : "Deleted 5151027b12542103e0e0999226c7d49cc9a82f78.png",
-                        data : data,
-                
+                        statusCode: 200,
+                        status: false,
+                        errMsg: "error getting students :" + err
                     });
-                });
-
-                context.succeed({ 
-                                
-                        
-                            });
-                */
-
-                
-
-                //destination_sessions
-
-                /*
-                connection.query("delete from fateweaver.destination_sessions where student_id = ?", [event.form.student_id], function (err, results, fields) {
+                }
+                //student_interests
+                connection.query("delete from fateweaver.student_interests where student_id = ?", [event.form.student_id], function (err, results, fields) {
                     if (err) {
                         context.succeed({
                             statusCode: 200,
@@ -102,8 +117,8 @@ exports.handler = (event, context, callback) => {
                             errMsg: "error getting students :" + err
                         });
                     }
-                    //mentor_assigned
-                    connection.query("delete from fateweaver.mentor_assigned where student_id = ?", [event.form.student_id], function (err, results, fields) {
+                    //student_mentor_links
+                    connection.query("delete from fateweaver.student_mentor_links where student_id = ?", [event.form.student_id], function (err, results, fields) {
                         if (err) {
                             context.succeed({
                                 statusCode: 200,
@@ -111,38 +126,32 @@ exports.handler = (event, context, callback) => {
                                 errMsg: "error getting students :" + err
                             });
                         }
-                        //student_files
-                        //For each file delete it from s3
-
-
-                    
+                        connection.query("delete from fateweaver.students where id = ?", [event.form.student_id], function (err, results, fields) {
+                            if (err) {
+                                context.succeed({
+                                    statusCode: 200,
+                                    status: false,
+                                    errMsg: "error getting students :" + err
+                                });
+                            }
+                        });
                     });
-                });*/
-
-                //destination_sessions--
-                //mentor_assigned--
-                //student_files*
-                //From s3*
-                //student_intrests
-                //student_mentor_links
-                //students
-
-
-
-
-            } else {
-                context.succeed({
-                    statusCode: 200,
-                    status: false,
-                    errMsg: "You dont have access to update students"
                 });
-            }
-
+            })
         });
-    });
+        
+
+    }
 
 
 }
 
+//destination_sessions--
+//mentor_assigned--
+//student_files*
+//From s3*
+//student_interests
+//student_mentor_links
+//students
 
 
