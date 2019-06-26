41f633a07c0853598c7b03ad55783f334ef7c98c
John <john@blooware.co.uk>
2019-06-26 16:43:22 +0100

    deactivate student swaps

--- fateweaverAdmin/Lambda/DeactivateStudent/index.js
+++ fateweaverAdmin/Lambda/DeactivateStudent/index.js
@@ -10,6 +10,7 @@ var connection = mysql.createConnection({
 });
 
 exports.handler = (event, context, callback) => {
+    var setActive;
     connection.query("select * from fateweaver.students where id = ? and school_id = (select school_id from fateweaver.admins where cognito_id = ?)", [event.form.student_id, event.account.sub], function (err, results, fields) {
         if (err) {
             console.log("Error getting tutor groups:", err);
@@ -20,10 +21,17 @@ exports.handler = (event, context, callback) => {
             });
         }
         if (results.length > 0) {
+
+            if(results[0].active == 0){
+                setActive = 1
+            } else{
+                setActive = 0
+            }
             var data = {
                 student_id: event.form.student_id,
                 added: new Date(Date.now()),
-                added_id: event.account.sub
+                added_id: event.account.sub,
+                active : setActive,
             }
             connection.query("insert into fateweaver. deactivation_log set ?", [data], function (err, results, fields) {
                 if (err) {
@@ -35,7 +43,7 @@ exports.handler = (event, context, callback) => {
                     });
                 }
 
-                connection.query("update fateweaver.students set active = 0 where id = ? ", [event.form.student_id], function (err, results, fields) {
+                connection.query("update fateweaver.students set active = ? where id = ? ", [setActive, event.form.student_id], function (err, results, fields) {
                     if (err) {
                         console.log("Error getting tutor groups:", err);
                         context.succeed({
@@ -44,11 +52,19 @@ exports.handler = (event, context, callback) => {
                             errMsg: "error getting Admin Account :" + err
                         });
                     }
+                    if(setActive == 0){
                     context.succeed({
                         statusCode: 200,
                         status: true,
                         message: "deactivated student"
                     });
+                    } else{
+                        context.succeed({
+                            statusCode: 200,
+                            status: true,
+                            message: "Reactivated student"
+                        });
+                }
                 });
 
 
