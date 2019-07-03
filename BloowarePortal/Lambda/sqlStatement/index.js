93329c853fbb658373fff398e74955adee7b923e
John <john@blooware.co.uk>
2019-07-03 16:43:22 +0100

    mysql statement

---
+++ BloowarePortal/Lambda/sqlStatement/index.js
@@ -0,0 +1,36 @@
+select  *, if ((select count(student_id) from fateweaver.deactivation_log where student_id = fateweaver.students.id ) > 1, 
+
+if(fateweaver.students.active = 1   ,(select DATE_FORMAT(date('1970-12-31 23:59:59') + interval -1* (
+
+( (select UNIX_TIMESTAMP(added)  - unix_timestamp())
++ 
+(select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where student_id = fateweaver.students.id and active = 1)
+)
+
+- (select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where active = 0)
+
+)
+ second,'%j days %Hh:%im:%ss') as time_Active2) ,
+ 
+
+(select DATE_FORMAT(date('1970-12-31 23:59:59') + interval -1* (
+
+(select UNIX_TIMESTAMP(added) 
++ 
+(select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where student_id = fateweaver.students.id and active = 1)
+)
+
+- (select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where active = 0)
+
+)
+ second,'%j days %Hh:%im:%ss') as time_Active2) 
+ )
+
+,
+if((select count(student_id) from fateweaver.deactivation_log where student_id = fateweaver.students.id ) = 1, 
+
+(select DATE_FORMAT(date('1970-12-31 23:59:59') + interval (UNIX_TIMESTAMP(now()) - UNIX_TIMESTAMP(added))  second,'%j days %Hh:%im:%ss') as time_Active1) 
+
+, (select DATE_FORMAT(date('1970-12-31 23:59:59') + interval (UNIX_TIMESTAMP(now()) - UNIX_TIMESTAMP(added))  second,'%j days %Hh:%im:%ss') as time_Active1) )
+
+) as ActiveTime from fateweaver.students 
