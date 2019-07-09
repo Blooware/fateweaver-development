90a6ac92bbbf41d2a08f0115f4b426acf8beac3a
John <john@blooware.co.uk>
2019-07-09 18:38:46 +0100

    implimented to blooware portal

--- BloowarePortal/Lambda/GrabSchool/index.js
+++ BloowarePortal/Lambda/GrabSchool/index.js
@@ -21,7 +21,7 @@ exports.handler = (event, context, callback) => {
             });
         }
         var schoolInfo = results;
-        connection.query("select * from (select fateweaver.students.id, given_name, family_name, dob, gender, postcode, upn, uln, tutor_group_id, pp, sen, fateweaver.students.added, fateweaver.students.active,	 if (fateweaver.students.year is not null , if((DATE_FORMAT(now(), '%m')) < 9, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year + 1),    (select if (DATE_FORMAT(dob, '%m') < 9, (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 4 ) , (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 5 ) ))) as yearGroup, fateweaver.students.id as student_id, fateweaver.tutor_groups.name as tutorGroup, (select aspiration from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1) as aspiration, (select if(length((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)) > 1,((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)),((select concat('(Unconfirmed) ', aspiration) from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)))) as destination from fateweaver.students left join fateweaver.tutor_groups on fateweaver.tutor_groups.id=fateweaver.students.tutor_group_id where fateweaver.students.school_id = ? and fateweaver.students.active = 1 ) as x where  yearGroup  < 13 ", [event.form.school_id], function (err, results, fields) {
+        connection.query("select  *, if ((select count(student_id) from fateweaver.deactivation_log where student_id = fateweaver.students.id ) >= 1, (if((SELECT active FROM fateweaver.deactivation_log where student_id = fateweaver.students.id order by added desc limit 1) = 0, (select DATE_FORMAT(date('1970-12-31 23:59:59') + interval -1* (if((select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where student_id = fateweaver.students.id and active = 1) is null , UNIX_TIMESTAMP(added) , (select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where student_id = fateweaver.students.id and active = 1) + unix_timestamp(added) )- (select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where active = 0 and student_id =  fateweaver.students.id)) second,'%j days %Hh:%im:%ss') as time_Active2) , (select DATE_FORMAT(date('1970-12-31 23:59:59') + interval -1* (( (select UNIX_TIMESTAMP(added)  - unix_timestamp())+ (select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where student_id = fateweaver.students.id and active = 1))- (select sum(UNIX_TIMESTAMP(added)) from fateweaver.deactivation_log where active = 0 and student_id =  fateweaver.students.id))second,'%j days %Hh:%im:%ss') as time_Active2))), (select DATE_FORMAT(date('1970-12-31 23:59:59') + interval (UNIX_TIMESTAMP(now()) - UNIX_TIMESTAMP(added))  second,'%j days %Hh:%im:%ss') as time_Active1))as timeActive from fateweaver.students where school_id = ? ", [event.form.school_id], function (err, results, fields) {
         
             if (err) {
                 console.log("Error getting tutor groups:", err);
@@ -32,14 +32,15 @@ exports.handler = (event, context, callback) => {
                     school_id : event.form.school_id
                 });
             }
-            console.log(results)
+            var studentList = results
 
 
             context.succeed({
                 statusCode: 200,
                 status: true,
                 body: schoolInfo,
-                chargable_students: results,
+                studentList : studentList,
+                chargable_students: "Not yet implimented",
             });
         });
     });
