380dd924c22867b5a6fc862dbec29a12cc30e377
John <john@blooware.co.uk>
2019-07-31 11:46:32 +0100

    old code merged back

--- fateweaverAdmin/Lambda/getStudents/index.js
+++ fateweaverAdmin/Lambda/getStudents/index.js
@@ -7,7 +7,57 @@ var connection = mysql.createConnection({
 });
 
 exports.handler = (event, context, callback) => {
-connection.query("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));", [event.account.sub], function (err, results, fields) {
+    
+    /*
+    // node native promisify
+    const query = util.promisify(connection.query).bind(connection);
+    var rows1;
+    var rows2;
+    //var totalRows = [{ "name":"Student1", "Interests" : [{"Name" : "Acting"}, {"Name" : "Dancing"}] } ,{"name":"Student2"}];
+    var totalRows = [{ "name":"Student1"}, {"name":"Student2"}];
+    console.log(totalRows);
+    console.log(event.account.sub);
+
+
+
+    (async () => {
+        try {
+            const rows = await query("select *, if (fateweaver.students.year is not null , if((DATE_FORMAT(now(), '%m')) < 9, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year + 1),    (select if (DATE_FORMAT(dob, '%m') < 9, (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 4 ) , (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 5 ) ))) as yearGroup, fateweaver.students.id as student_id, fateweaver.tutor_groups.name as tutorGroup, (select aspiration from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1) as aspiration, (select if(length((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)) > 1,((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)),((select concat('(Unconfirmed) ', aspiration) from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)))) as destination from fateweaver.students left join fateweaver.tutor_groups on fateweaver.tutor_groups.id=fateweaver.students.tutor_group_id where fateweaver.students.school_id = (select school_id from fateweaver.admins where cognito_id = ' " + event.account.sub + "' limit 1)");
+            console.log(rows);
+            rows1 = rows;
+            //totalRows.push(rows);
+            console.log("rows1");
+            (async () => {
+                try {
+                    const rows = await query('SELECT interest_id, name, fateweaver.student_interests.added FROM fateweaver.student_interests left join fateweaver.interests on fateweaver.student_interests.interest_id = fateweaver.interests.id where fateweaver.student_interests.student_id = 25 ;');
+                    console.log(rows);
+                    //totalRows.push(rows);
+                    rows2 = rows;
+                    console.log("rows2");
+                    console.log(rows1);
+                    console.log(rows2);
+                    totalRows[0].Interests = rows;
+                    console.log(totalRows);
+
+                    
+
+
+
+
+                } finally {
+                    console.log("Finally1");
+                    
+                    connection.end();
+
+
+                }
+            })()
+        } finally {
+            console.log("Finally2");
+            //connection.end();
+        }
+    })()
+    */
     
     connection.query("select given_name, family_name, dob, gender, postcode, upn, uln, tutor_group_id, pp, sen, fateweaver.students.added, fateweaver.students.csv, if (fateweaver.students.year is not null , if((DATE_FORMAT(now(), '%m')) < 9, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year, DATE_FORMAT(now(), '%y') - DATE_FORMAT(year_added, '%y') + fateweaver.students.year + 1),    (select if (DATE_FORMAT(dob, '%m') < 9, (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 4 ) , (DATE_FORMAT(NOW(), '%Y') - DATE_FORMAT(dob, '%Y') - (DATE_FORMAT(NOW(), '00-%m-%d') < DATE_FORMAT(dob, '00-%m-%d')) - 5 ) ))) as yearGroup, fateweaver.students.id as student_id, fateweaver.tutor_groups.name as tutorGroup, (select aspiration from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1) as aspiration, (select if(length((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)) > 1,((select confirmed_place from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)),((select concat('(Unconfirmed) ', aspiration) from fateweaver.destination_sessions where student_id = fateweaver.students.id order by id desc limit 1)))) as destination from fateweaver.students left join fateweaver.tutor_groups on fateweaver.tutor_groups.id=fateweaver.students.tutor_group_id where fateweaver.students.school_id = (select school_id from fateweaver.admins where cognito_id = ? limit 1);", [event.account.sub], function (err, results, fields) {
         if (err) {
@@ -38,7 +88,6 @@ connection.query("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GRO
         });
 
 
-    });
     });
 }
 
