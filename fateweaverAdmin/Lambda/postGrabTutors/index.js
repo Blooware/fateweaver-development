55a45d41595302562f3d36774c56d15c695a0ca9
John <john@blooware.co.uk>
2019-08-28 13:05:08 +0100

    tutor page info

--- fateweaverAdmin/Lambda/postGrabTutors/index.js
+++ fateweaverAdmin/Lambda/postGrabTutors/index.js
@@ -1,3 +1,5 @@
+//fateweaverAdmin-postGrabTutors
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     "host": process.env.host,
@@ -7,15 +9,14 @@ var connection = mysql.createConnection({
 });
 
 exports.handler = (event, context, callback) => {
-
-    connection.query("SET SESSION sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));", [event.account.sub], function (err, results, fields) {
-        connection.query("SELECT id, first_name, last_name, role, linkedin, added FROM fateweaver.mentors where school_id = (select school_id from fateweaver.admins where cognito_id = ?) order by id desc", [event.account.sub], function (err, results, fields) {
+    //callback(null,event);
+        connection.query("SELECT id, title, first_name, second_name, added, email FROM fateweaver.tutors where school_id = (select school_id from fateweaver.admins where cognito_id = ?)", [event.account.sub], function (err, results, fields) {
             if (err) {
                 console.log("Error getting tutor groups:", err);
                 context.succeed({
                     statusCode: 200,
                     status: false,
-                    errMsg: "error adding that mentor err :" + err
+                    errMsg: "error getting students :" + err
                 });
             }
             context.succeed({
@@ -24,13 +25,4 @@ exports.handler = (event, context, callback) => {
                 body: results
             });
         });
-    });
 }
-
-
-
-
-
-
-
-
